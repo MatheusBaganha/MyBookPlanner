d5d9393781b035f8974c46d7e08b99e614fa96a1
MatheusBaganha <matheus.baganha1@gmail.com>
2023-06-24 18:39:38 -0300

    feat: User books statistics

---
+++ MyBookPlannerAPI/Controllers/UserBooksStatisticsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyBookPlannerAPI.Data;
+using MyBookPlannerAPI.ViewModels;
+using MyBookPlannerAPI.ViewModels.UserBooks;
+
+namespace MyBookPlannerAPI.Controllers
+{
+    public class UserBooksStatisticsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/user-book/{idUser:int}/statistics")]
+        public async Task<IActionResult> GetUserStatistics([FromServices] CatalogDataContext context, [FromRoute] int idUser)
+        {
+            try
+            {
+                var userBooks = await context.UserBooks.Where(x => x.IdUser == idUser).ToListAsync();
+
+                if(userBooks.Count == 0 || userBooks == null)
+                {
+                    return NotFound(new ResultViewModel<UserBooksStatisticsViewModel>("User books was not found."));
+                }
+
+                var readingBooks = userBooks.Count(x => x.ReadingStatus.ToUpper() == "LENDO");
+                var readedBooks = userBooks.Count(x => x.ReadingStatus.ToUpper() == "LIDO");
+                var wishToReadBooks = userBooks.Count(x => x.ReadingStatus.ToUpper() == "DESEJO");
+
+                var statistics = new UserBooksStatisticsViewModel
+                {
+                    reading = readingBooks,
+                    readed = readedBooks,
+                    wishToRead = wishToReadBooks
+                };
+
+                return Ok(new ResultViewModel<UserBooksStatisticsViewModel>(statistics));
+            } 
+            catch (Exception ex)
+            {
+                return BadRequest(new ResultViewModel<UserBooksStatisticsViewModel>("Internal error."));
+            }
+            
+        }
+    }
+}
---
+++ MyBookPlannerAPI/ViewModels/UserBooks/UserBooksStatisticsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBookPlannerAPI.ViewModels.UserBooks
+{
+    public class UserBooksStatisticsViewModel
+    {
+        public int reading { get; set; }
+        public int readed { get; set; }
+        public int wishToRead { get; set; }
+    }
+}
